b952896edc961661c8f1140873f51a0d225c1ab6
Zachary Widder <zwidder@cisco.com>
2016-05-15 11:47:16 -0700

    added files from wrong folder, removing them al

--- APM - Start VS 2015 ASP 4x/APM/app/main.ts
+++
@@ -1,7 +0,0 @@
-///<reference path="../typings/browser.d.ts"/>
-import { bootstrap } from 'angular2/platform/browser';
-
-// Our main component
-import { AppComponent } from './app.component';
-
-bootstrap(AppComponent);
